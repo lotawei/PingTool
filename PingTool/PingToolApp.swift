dec7f34402b5bbcec02dd3da0da3b0008862efe7
lotawei <1286594907@qq.com>
2024-12-30 13:49:27 +0800

    Initial Commit

---
+++ PingTool/PingToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingToolApp.swift
+//  PingTool
+//
+//  Created by work on 2024/12/30.
+//
+
+import SwiftUI
+
+@main
+struct PingToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
